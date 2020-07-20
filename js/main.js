37bcf1de4e7c9965e90a2cac88e64faaa38d7273
Andrey <Andrey-0902@yandex.ru>
2020-07-20 10:59:21 +0800

    Добавляет начало работы с проектом

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var mapPins = document.querySelector('.map__pins');
+var card = document.querySelector('.card');
+var renderSuggestion = function() {
+  
+}
